821cb2189b331230d9a666ab905b25763c1c618e
John Doe <eva.lm1810@gmail.com>
2021-04-12 11:31:26 +0200

    Updates

---
+++ precache-manifest.579325bc1793c55491c4d02967f9c038.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac10216c62c5ca8330364415ad969a5",
+    "url": "/directorio-personas/index.html"
+  },
+  {
+    "revision": "9b14a94f98c62958cad8",
+    "url": "/directorio-personas/static/css/main.bcd884ee.chunk.css"
+  },
+  {
+    "revision": "6a99c98a1e0d91d3441c",
+    "url": "/directorio-personas/static/js/2.cafe9e5c.chunk.js"
+  },
+  {
+    "revision": "9b14a94f98c62958cad8",
+    "url": "/directorio-personas/static/js/main.d0be515d.chunk.js"
+  },
+  {
+    "revision": "53512367f7e973dd3636",
+    "url": "/directorio-personas/static/js/runtime~main.110742fd.js"
+  }
+]);
